276bc176dea09f3901d34b6536a974d4122ef21f
Sunada Yuki <183799910+Sunachan1245@users.noreply.github.com>
2025-07-07 16:05:28 +0900

    ハンバーガーメニューの修正
    ハンバーガーメニューを大体直しました

--- js/script.js
+++ js/script.js
@@ -1,15 +1,31 @@
 window.addEventListener('load', function () {
-    var $button = this.document.querySelector('.toggle-menu-button');
-    var $menu = this.document.querySelector('.header-site-menu');
+    var $button = document.querySelector('.toggle-menu-button');
+    var $menu = document.querySelector('.header-site-menu');
 
-    $button.addEventListener('click', function () {
-        if ($menu.classList.contains('is-show')) {
-            $menu.classList.remove('is-show');
-        }
-        else {
-            $menu.classList.add('is-show');
-        }
-    });
+    // ハンバーガーメニューボタンが存在する場合のみイベントリスナーを追加
+    if ($button && $menu) {
+        $button.addEventListener('click', function () {
+            if ($menu.classList.contains('is-show')) {
+                $menu.classList.remove('is-show');
+            } else {
+                $menu.classList.add('is-show');
+            }
+        });
+
+        // メニュー外をクリックした時にメニューを閉じる
+        document.addEventListener('click', function(e) {
+            if (!$button.contains(e.target) && !$menu.contains(e.target)) {
+                $menu.classList.remove('is-show');
+            }
+        });
+
+        // ウィンドウサイズが変更された時にメニューを閉じる
+        window.addEventListener('resize', function() {
+            if (window.innerWidth > 768) {
+                $menu.classList.remove('is-show');
+            }
+        });
+    }
 });
 
 
